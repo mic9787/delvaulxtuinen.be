9cb403c0c5abf7d02a55bdd307dc67c27122a4bc
Michael Delvaulx <michaeldelvaulx@gmail.com>
2016-07-27 16:58:12 +0200

    no message

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,3 +7,24 @@ $( document ).ajaxComplete(function( event, xhr, settings ) {
     });
   }
 });
+
+// DEV only
+$(function () {
+    // header
+    jQuery.get("resources/html/components/header.html", function (result) {
+        jQuery("#header-placeholder").replaceWith(result);
+    });
+    // hero header
+    jQuery.get("resources/html/components/footer.html", function (result) {
+        jQuery("#footer-placeholder").replaceWith(result);
+    });
+    // homepage
+    jQuery.get("resources/html/components/home.html", function (result) {
+        jQuery("#home-placeholder").replaceWith(result);
+    });
+    // hero header
+    jQuery.get("resources/html/components/hero.html", function (result) {
+        jQuery("#hero-placeholder").replaceWith(result);
+    });
+    
+});
